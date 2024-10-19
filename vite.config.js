51079caf746a562a1638a203772b01c11d7446a5
jimothy-timothy <alec1.zhou@gmail.com>
2024-10-19 18:06:11 -0400

    Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/azhou555.github.io",
+  base: "/azhou555.github.io/",
   plugins: [
     vue(),
   ],
